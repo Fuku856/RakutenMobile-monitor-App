ed0655d50e34535358954287b5cd42d2e7661b7c
Fuku856 <hachi56kiku56@gmail.com>
2025-12-29 13:38:34 +0900

    feat: AndroidプロジェクトのGradle設定を初期設定しました。

--- RakutenMonitorAndroid/settings.gradle.kts
+++ RakutenMonitorAndroid/settings.gradle.kts
@@ -1,15 +1,17 @@
 pluginManagement {
     repositories {
         google()
-        mavenCentral()
+        maven { url = uri("https://repo1.maven.org/maven2/") }
         gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral()
+        maven { url = uri("https://repo1.maven.org/maven2/") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
